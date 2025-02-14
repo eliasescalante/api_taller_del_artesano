526eb6ea8b6d0a42f86e4af5f76313cf8d22c6da
eliasescalante <eliasw.escalante@gmail.com>
2025-02-14 18:45:34 -0300

    update to db.js

--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
+    password: {
+        type: String,
+        required:true,
+    }
 });
 
 export default mongoose.model("User", userSchema);
